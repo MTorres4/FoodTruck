b9ff27e25b5ad3fddfd9a836fc0d12631e539760
cherr000 <chudue@hotmail.com>
2017-04-04 11:59:46 -0500

    Added CAlENDAR

--- FoodTruck/Controllers/HomeController.cs
+++ FoodTruck/Controllers/HomeController.cs
@@ -13,6 +13,71 @@ namespace FoodTruck.Controllers
             return View();
         }
 
+        public JsonResult GetEvents()
+        {
+            //Here MyDatabaseEntities is our entity datacontext (see Step 4)
+            using (Entities dc = new Entities())
+            {
+                var v = dc.Events.OrderBy(a => a.StartAt).ToList();
+                return new JsonResult { Data = v, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
+        }
+
+        //Action for Save event
+        [HttpPost]
+        public JsonResult SaveEvent(Event evt)
+        {
+            bool status = false;
+            using (Entities dc = new Entities())
+            {
+                if (evt.EndAt != null && evt.StartAt.TimeOfDay == new TimeSpan(0, 0, 0) &&
+                    evt.EndAt.Value.TimeOfDay == new TimeSpan(0, 0, 0))
+                {
+                    evt.IsFullDay = true;
+                }
+                else
+                {
+                    evt.IsFullDay = false;
+                }
+                if (evt.EventID > 0)
+                {
+                    var v = dc.Events.Where(a => a.EventID.Equals(evt.EventID)).FirstOrDefault();
+                    if (v != null)
+                    {
+                        v.Title = evt.Title;
+                        v.Description = evt.Description;
+                        v.StartAt = evt.StartAt;
+                        v.EndAt = evt.EndAt;
+                        v.IsFullDay = evt.IsFullDay;
+                    }
+                }
+                else
+                {
+                    dc.Events.Add(evt);
+                }
+                dc.SaveChanges();
+                status = true;
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+
+        [HttpPost]
+        public JsonResult DeleteEvent(int eventID)
+        {
+            bool status = false;
+            using (Entities dc = new Entities())
+            {
+                var v = dc.Events.Where(a => a.EventID.Equals(eventID)).FirstOrDefault();
+                if (v != null)
+                {
+                    dc.Events.Remove(v);
+                    dc.SaveChanges();
+                    status = true;
+                }
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
