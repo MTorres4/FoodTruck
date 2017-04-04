b799dcfe86c89e29d4579917d0eb5ef5c8ba578b
Marissa <Marissa.Bart@cuw.edu>
2017-04-04 11:08:39 -0500

    Added basic Views based on user roles

---
+++ FoodTruck/Controllers/EmployeeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FoodTruck.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Schedule()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ FoodTruck/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using FoodTruck.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FoodTruck.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        ApplicationDbContext db;
+
+        public LoginController()
+        {
+            db = new ApplicationDbContext();
+        }
+
+
+        // GET: Login
+        public ActionResult Index()
+        {
+            if(User.IsInRole("Admin"))
+            {
+                return RedirectToAction("Index", "Admin");
+            }
+            if(User.IsInRole("Employee"))
+            {
+                return RedirectToAction("Index", "Employee");
+            }
+
+            return View();
+        }
+    }
+}
